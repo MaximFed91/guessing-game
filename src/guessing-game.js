c01cb93acc1de7d896faa5c09e8474798d3b6713
Maksim <honestliar91@gmail.com>
2021-08-05 22:36:07 +0300

    init

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        
+    }
 
     setRange(min, max) {
+        this.min = min;
+        this.max = max;
+        this.gues = Math.round((this.max-this.min)/2) + this.min;
+        
 
     }
 
     guess() {
-
+        return this.gues;
     }
 
     lower() {
-
+        this.max = this.gues-1;
+        this.gues = Math.round((this.max-this.min)/2) + this.min;
     }
 
     greater() {
-
+        this.min = this.gues + 1 ;
+        this.gues = Math.round((this.max-this.min)/2) + this.min;
     }
 }
 
